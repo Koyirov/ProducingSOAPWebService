8bb192e9cb62c6f15df4bc994738b79389c6b724
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-29 06:35:16 +0200

    initial commit

---
+++ src/main/java/com/example/producingsoapwebservice/ProducingSOAPWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.producingsoapwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducingSOAPWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducingSOAPWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/producingsoapwebservice/ProducingSoapWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.producingsoapwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducingSoapWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
